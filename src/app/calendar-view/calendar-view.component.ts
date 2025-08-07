6b430586cba5575be3d394e2af0dbe8fa45ecdc8
Piyush7744 <piyush@gmail.com>
2025-08-07 12:57:38 +0530

    user event completion from DialogBox

--- src/app/calendar-view/calendar-view.component.ts
+++ src/app/calendar-view/calendar-view.component.ts
@@ -6,6 +6,15 @@ import { formatDate } from '@angular/common';
 import { DialogComponent } from '../dialog/dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 
+
+interface formData {
+  title: string,
+  amount: number,
+  description: string,
+  type: string,
+  date: string,
+}
+
 @Component({
   selector: 'app-calendar-view',
   templateUrl: './calendar-view.component.html',
@@ -13,90 +22,97 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class CalendarViewComponent implements OnInit {
   formOpen: boolean = false;
-  form = {
+  form: formData = {
     title: '',
     date: '',
     type: '',
     amount: 0,
     description: ''
   }
-  calendarOptions: CalendarOptions;
-  constructor(public dialog: MatDialog) {
-    this.calendarOptions = {
-      plugins: [dayGridPlugin, interactionPlugin],
-      initialView: 'dayGridMonth',
-      headerToolbar: {
-        left: 'prev,today,next',
-        center: 'title',
-        right: 'dayGridMonth,dayGridWeek,dayGridDay myCustomButton'
-      },
-      height: '595px',
-      weekends: true,
-      events: [
-        {
-          title: 'Event 1', date: '2025-08-10', extendedProps: { description: 'Discuss project progress', amount: 2000, status: 'pending' }
-        },
-        {
-          title: 'Event 2', date: '2025-08-11', extendedProps: { description: 'Discuss project progress', amount: 2000, status: 'completed' }
-        },
-        {
-          title: 'Event 1', date: '2025-08-12', extendedProps: { description: 'Discuss project progress', amount: 2000, status: 'pending' }
-        },
 
-      ],
-      // In event i am showing amount as well with title initially it was only title
-      eventContent: (arg) => {
-        let customHtml = `<div>${arg.event.title}</div>`;
-        if (arg.event.extendedProps['description']) {
-          customHtml += `<div class="event-description">${arg.event.extendedProps['amount']}</div>`;
-        }
-        return { html: customHtml };
+  calendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,today,next',
+      center: 'title',
+      right: 'dayGridMonth,dayGridWeek,dayGridDay myCustomButton'
+    },
+    height: '595px',
+    weekends: true,
+    events: [
+      {
+        title: 'Event 1', date: '2025-08-10', extendedProps: { description: 'Discuss project progress', amount: 2000, status: 'pending' }
       },
-      // adding my custom button in header.
-      customButtons: {
-        myCustomButton: {
-          text: 'Add Event',
-          click: () => {
-            this.formOpen = true
-          }
-        }
+      {
+        title: 'Event 2', date: '2025-08-11', extendedProps: { description: 'Discuss project progress', amount: 2000, status: 'completed' }
       },
-      // this is for applying event color based on status of event.
-      eventDidMount: (info) => {
-        if (info.event.extendedProps['status'] === 'pending') {
-          info.el.style.backgroundColor = 'red';
-        } else if (info.event.extendedProps['status'] === 'completed') {
-          info.el.style.backgroundColor = 'green';
-        }
+      {
+        title: 'Event 1', date: '2025-08-12', extendedProps: { description: 'Discuss project progress', amount: 2000, status: 'pending' }
       },
-      eventClick: this.handleEventClick.bind(this)
-    };
 
-  }
+    ],
+    // In event i am showing amount as well with title initially it was only title
+    eventContent: (arg) => {
+      let customHtml = `<div>${arg.event.title}</div>`;
+      if (arg.event.extendedProps['description']) {
+        customHtml += `<div class="event-description">${arg.event.extendedProps['amount']}</div>`;
+      }
+      return { html: customHtml };
+    },
+    // adding my custom button in header.
+    customButtons: {
+      myCustomButton: {
+        text: 'Add Event',
+        click: () => {
+          this.formOpen = true
+        }
+      }
+    },
+    // this is for applying event color based on status of event.
+    eventDidMount: (info) => {
+      if (info.event.extendedProps['status'] === 'pending') {
+        info.el.style.backgroundColor = 'red';
+      } else if (info.event.extendedProps['status'] === 'completed') {
+        info.el.style.backgroundColor = 'green';
+      }
+    },
+    eventClick: this.handleEventClick.bind(this)
+  };
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
-
     if (localStorage.getItem('calendar')) {
       const data = localStorage.getItem('calendar');
-      console.log(JSON.parse(data));
-
       this.calendarOptions.events = JSON.parse(data).events;
     }
-
   }
+
   // this function is for handling event click and it will open a dialogBox which is my
   // DialogBox Component and it is subscribing to its reference so whenever a closeDialogBox operation is
   // occured at dialogBox it will notify about that.
   handleEventClick(args: any) {
     console.log(args.event);
     let dialogRef = this.dialog.open(DialogComponent, { data: args });
+    // this will get triggered when user click completed button.
     dialogRef.afterClosed().subscribe(res => {
-      console.log("Dialog box closed");
+      // storing all the events in data variable and updating the status of currentEvent that user just completed
+      let data = this.calendarOptions.events.filter(item => {
+        if (item.title != res.event.title) {
+          return item;
+        } else {
+          // changing status of currentEvent
+          return item.extendedProps.status = 'completed';
+        }
+      })
+      // again updating value of events and localStorage after changing status
+      this.calendarOptions.events = data;
+      localStorage.setItem('calendar',JSON.stringify(this.calendarOptions));
     })
   }
 
   // this is to close addEvent form if user want to dont add any event.
-
   closeForm() {
     this.formOpen = false;
   }
@@ -105,8 +121,6 @@ export class CalendarViewComponent implements OnInit {
   // update the calendarOptions and here i am storing calendarOptions in my localStorage so if user by mistakely
   // refreshes the page then also his/her event will remain unChanged.
   submitForm() {
-
-
     const date = new Date(this.form.date);
     const date2 = formatDate(date, 'yyyy-MM-dd', 'en-US')
     // taking current events data.
@@ -120,5 +134,4 @@ export class CalendarViewComponent implements OnInit {
     // closing the form.
     this.formOpen = false;
   }
-
 }
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -7,11 +7,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class DialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public eventData: any, private dialogRef: MatDialogRef<DialogComponent>) { }
+  // this function will close the dialogBox.
   closeDialog() {
     this.dialogRef.close();
   }
-  completed(){
-    console.log(this.eventData);
-    
+  // if user click complete button this function gets trigger.
+  completed() {
+    // sending the current event back to the calendar Component and will change it's status to completed.
+    this.dialogRef.close(this.eventData);
   }
 }
